349e84c5947d9bfeb06071fac6e79258d9fc2c8b
Sclarke5 <sclarke5555@gmail.com>
2018-03-21 15:00:46 -0400

    completes exercise 3

--- exercises.rb
+++ exercises.rb
@@ -9,12 +9,14 @@ movie_years = {godfather: 1972, scarface: 1983, memento: 2000}
 city_population = {toronto: 2700000, rio: 6500000, shanghai: 24000000}
 hash_ages = {sean: 27, tricia: 25, robyn: 30, kristen: 28, taylor: 22}
 
+# Exercise 1
 puts coin_toss
 puts string_colors[0]
 puts array_ages.sort
 array_ages << 0
 puts movie_years[:godfather]
 
+# Exercise 2
 puts string_colors[-1]
 city_population[:tokyo] = 2000000
 coin_toss = coin_toss.reverse
@@ -23,3 +25,15 @@ puts city_population[:rio]
 fav_artists.each do |artist|
   puts "#{artist} is one of my favourite artists"
 end
+
+# Exercise 3
+puts fav_artists [0..1]
+movie_years.each do |movie, year|
+  puts "#{movie} came out in #{year}"
+end
+array_ages = array_ages.sort.reverse
+puts array_ages
+movie_years['beauty and the beast'] = [1991, 2017]
+puts movie_years
+
+# Exercise 4
