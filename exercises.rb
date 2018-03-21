7bd28a484846e9aa6edb5efdedeaafe94ea78038
Sclarke5 <sclarke5555@gmail.com>
2018-03-21 14:18:21 -0400

    creates arrays and hashes

---
+++ exercises.rb
@@ -0,0 +1,10 @@
+string_colors = ['red', 'blue', 'green', 'black']
+array_ages = [27, 25, 30, 28, 22]
+coin_toss = [true, true, false, false, true]
+fav_artists = ['kanye', 'prince', 'miley']
+sym_colors = [:red, :blue, :green, :black]
+
+words_def = {car: 'a metal box that travels on four wheels', desk: 'a flat surface for working', phone: 'a device used for communication'}
+movie_years = {godfather: 1972, scarface: 1983, memento: 2000}
+city_population = {toronto: 2700000, rio: 6500000, shanghai: 24000000}
+hash_ages = {sean: 27, tricia: 25, robyn: 30, kristen: 28, taylor: 22}
